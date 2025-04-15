11f1ebb90f02386267109c9cf3a3c6247fbee464
tactition <zahidnazir330@gmail.com>
2025-04-15 18:31:06 +0530

    added the question answer

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ from Zahid.bot import StreamBot
 from Zahid.utils.keepalive import ping_server  # Your ping script imported here
 from Zahid.bot.clients import initialize_clients
 from plugins.ArticlesQuotes import schedule_daily_quotes, schedule_daily_articles
-from plugins.facts import schedule_facts
+from plugins.facts import schedule_facts, schedule_trivia
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
@@ -75,6 +75,7 @@ async def start():
     # schedule_daily_quotes(StreamBot)  
     # schedule_daily_articles(StreamBot)
     schedule_facts(StreamBot) 
+    schedule_trivia(StreamBot)
 
     
     if CLONE_MODE == True:
